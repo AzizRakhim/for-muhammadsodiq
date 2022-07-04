414aad497b1d915ee334606e051a835feca106ff
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-07-04 14:30:04 +0500

    added list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,110 @@
-let map = document.querySelector(".map-svg .map-path-one");
+const objMap = [{
+  id: 1,
+  name: "toshkent",
+  title: "toshkent shahar",
+  day: "6 kun",
+  study: "9:00-18:00",
+  teachers: "3",
+  students: "20",
+},
+{
+  id: 2,
+  name: "xorazm",
+  title: "Samarqand shahar",
+  day: "6 kun",
+  study: "9:00-18:00",
+  teachers: "3",
+  students: "20",
+},
+{
+  id: 3,
+  name: "buxoro",
+  title: "Buxoro shahar",
+  day: "6 kun",
+  study: "9:00-18:00",
+  teachers: "3",
+  students: "20",
+},
+{
+  id: 4,
+  name: "qoraqolpakston",
+  title: "Andijon shahar",
+  day: "6 kun",
+  study: "9:00-18:00",
+  teachers: "3",
+  students: "20",
+},
+];
 
-map.addEventListener("click", () => {
-  console.log("ok");
-})
+let list = document.querySelector(".list");
+
+let buxoro = document.querySelector(".map-svg .buxoro");
+let xorazm = document.querySelector(".map-svg .xorazm");
+let qoraqolpakston = document.querySelector(".map-svg .qoraqolpakstan");
+
+buxoro.addEventListener("click", () => showData(3));
+xorazm.addEventListener("click", () => showData(2));
+qoraqolpakston.addEventListener("click", () => showData(4));
+
+function showData(num) {
+  list.innerHTML = ""
+  objMap.forEach((item) => {
+    if(item.id == num){
+      let elItem = document.createElement("div");
+      elItem.className = "hududiy__info-div";
+      elItem.innerHTML = `
+                <div class="hududiy__info-title">
+                  <p class="hududiy__info-t">
+                    ${item.title}
+                  </p>
+                </div>
+                <div class="hududiy__info d-flex mb-3">
+                  <div class="hududiy__day1 d-flex flex-column">
+                    <span class="hududiy__info-span">
+                    ${item.day}
+                    </span>
+                    <p class="hududiy__info-text">
+                      Xafta kunlari
+                    </p>
+                  </div>
+                  <div class="hududiy__day d-flex flex-column">
+                    <span class="hududiy__info-span">
+                    ${item.study}
+                    </span>
+                    <p class="hududiy__info-text">
+                      O’quv-mashg’ulot vaqti
+                    </p>
+                  </div>
+                </div>
+                <div class="hududiy__info d-flex">
+                  <div class="hududiy__day2 d-flex flex-column">
+                    <span class="hududiy__info-span">
+                    ${item.teachers}
+                    </span>
+                    <p class="hududiy__info-text">
+                      Murabbiylar
+                    </p>
+                  </div>
+                  <div class="hududiy__day d-flex flex-column">
+                    <span class="hududiy__info-span">
+                    ${item.students}
+                    </span>
+                    <p class="hududiy__info-text">
+                      O’quvchilar
+                    </p>
+                  </div>
+                </div>
+                <a class="text-deco" href="/boglanish.html">
+                  <button class="heaeder__btn1 d-flex align-items-center ms-5">
+                    bog’lanish
+                    <i class='bx bx-right-arrow-alt header__btn-icon'></i>
+                  </button>
+                </a>
+        `;
+  
+      list.appendChild(elItem);
+      console.log(list);
+    }
+
+  })
+}
